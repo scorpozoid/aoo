5582e2e196ca19b7dba98f2ea1832066c2ab6446
scorpozoid <scorpozoid@gmail.com>
2018-01-16 01:02:22 +0300

    [+] chmog +x aoosort.sh

--- aoosort/aoosort.sh
+++ aoosort/aoosort.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-python aoosort.py -i ~/media/art -i ~/media/pic -x ~/media/boo -o ~/temp/aoosort --mode=touch
+# python aoosort.py -i ~/media/art -i ~/media/pic -x ~/media/boo -o ~/temp/aoosort --mode=touch
 
+python aoosort.py -i /media/$USER/SBP2T1/
 # EOF
